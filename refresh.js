05b3a13004f04b46c06ca6cc3e41935eb2d8a7eb
Earl Duque-HomePC <earl@earlduque.com>
2022-07-06 16:44:15 -0700

    comments

--- refresh.js
+++ refresh.js
@@ -3,30 +3,29 @@ var script_name = "early.script";
 var script_ram = getScriptRam(script_name);
 
 // Array of all servers that don't need any ports opened
-// to gain root access. These have 16 GB of RAM
 var servers0Port = ["n00dles",
-                    "foodnstuff",
-                    "sigma-cosmetics",
-                    "joesguns",
-                    "nectar-net",
-                    "hong-fang-tea",
-                    "harakiri-sushi"];
+    "foodnstuff",
+    "sigma-cosmetics",
+    "joesguns",
+    "nectar-net",
+    "hong-fang-tea",
+    "harakiri-sushi"];
 
 // Array of all servers that only need 1 port opened
-// to gain root access. These have 32 GB of RAM
 var servers1Port = ["neo-net",
-                    "zer0",
-                    "max-hardware",
-                    "iron-gym"];
+    "zer0",
+    "max-hardware",
+    "iron-gym"];
 
 // Array of all servers that need 2 ports opened
 var servers2Port = ["silver-helix",
-                    "phantasy",
-                    "omega-net"];
+    "phantasy",
+    "omega-net"];
 
+// Array of all servers that have no money but have ram for exploiting
 var moneyless_servers = ["home",
-                         "CSEC",
-                         "avmnite-02h"];
+    "CSEC",
+    "avmnite-02h"];
 
 // Copy our scripts onto each server that requires 0 ports
 // to gain root access. Then use nuke() to gain admin access and
@@ -41,6 +40,7 @@ for (var i in servers0Port) {
 
 // Wait until we acquire the "BruteSSH.exe" program
 while (!fileExists("BruteSSH.exe")) {
+    run_moneyless();
     sleep(60000);
 }
 
@@ -56,13 +56,17 @@ for (var i in servers1Port) {
     exec(script_name, serv, Math.floor(getServerMaxRam(serv) / script_ram));
 }
 
+// cancel the "waiting" script
+if (scriptRunning("post-refresh.script", "home")) scriptKill("post-refresh.script", "home");
+
 // Wait until we acquire the "BruteSSH.exe" program
 while (!fileExists("FTPCrack.exe")) {
+    run_moneyless();
     sleep(60000);
 }
 
 // Copy our scripts onto each server that requires 1 port
-// to gain root access. Then use brutessh() and nuke()
+// to gain root access. Then use brutessh(), ftpcrack(), and nuke()
 // to gain admin access and run the scripts.
 for (var i in servers2Port) {
     var serv = servers2Port[i];
@@ -74,13 +78,25 @@ for (var i in servers2Port) {
     exec(script_name, serv, Math.floor(getServerMaxRam(serv) / script_ram));
 }
 
-for (var i in moneyless_servers){
-    var serv = moneyless_servers[i];
-    
-    scp("post-refresh.script", serv);
-    brutessh(serv);
-    ftpcrack(serv);
-    nuke(serv);
-    if (moneyless_servers[i] == "home") exec("post-refresh.script", moneyless_servers[i], (Math.floor(getServerMaxRam(moneyless_servers[i]) - getScriptRam("refresh.script")) / getScriptRam("post-refresh.script")));
-    else exec("post-refresh.script", moneyless_servers[i], Math.floor(getServerMaxRam(moneyless_servers[i]) / getScriptRam("post-refresh.script")));
+// cancel the "waiting" script
+if (scriptRunning("post-refresh.script", "home")) scriptKill("post-refresh.script", "home");
+run_moneyless();
+
+// fills the moneyless servers' remaining ram to put them to work
+function run_moneyless() {
+    for (var i in moneyless_servers) {
+        var serv = moneyless_servers[i];
+
+        scp("post-refresh.script", serv);
+        var can_nuke = true;
+        if (getServerNumPortsRequired(serv) > 0 && fileExists("BruteSSH.exe")) {
+            brutessh(serv);
+            if (getServerNumPortsRequired(serv) > 1 && fileExists("FTPCrack.exe")) {
+                ftpcrack(serv);
+            } else can_nuke = false;
+        } else can_nuke = false;
+        if (getServerNumPortsRequired(serv) == 0 || can_nuke) nuke(serv);
+        if (moneyless_servers[i] == "home") exec("post-refresh.script", moneyless_servers[i], (Math.floor(getServerMaxRam(moneyless_servers[i]) - getScriptRam("refresh.script")) / getScriptRam("post-refresh.script")));
+        else exec("post-refresh.script", moneyless_servers[i], Math.floor(getServerMaxRam(moneyless_servers[i]) / getScriptRam("post-refresh.script")));
+    }
 }
